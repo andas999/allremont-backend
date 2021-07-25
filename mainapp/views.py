3a72df053cd6572cc70a266d83c0e7c1ba00dbf1
andas999 <andas999@mail.ru>
2021-07-25 13:25:04 +0600

    Create views.py

---
+++ mainapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+	context = {
+		'posts':posts
+	}
+	return render(request, 'home.html',context)
+
+def about(request):
+	return render(request, 'about.html', {'title': 'About'})
