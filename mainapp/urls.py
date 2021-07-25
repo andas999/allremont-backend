3290b5be3dfeb48101a43f26e21db64c3230443c
andas999 <andas999@mail.ru>
2021-07-25 13:04:12 +0600

    Update urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('api/', include('client.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('about/', views.about, name='blog-about'),
 
 ]
 urlpatterns += [
